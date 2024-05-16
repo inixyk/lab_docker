04624392a299a39df9fd3f4609545a26d37e11ba
yanuar <yanuar.hadiyanto@gmail.com>
2024-05-16 12:07:04 +0700

    typo

--- microsvc/cmd.sh
+++ microsvc/cmd.sh
@@ -5,7 +5,7 @@ if [ "$ENV" = 'DEV' ]; then
   exec python "aplikasi.py"
 elif [ "$ENV" = 'UNIT' ]; then
   echo "Unit Tests"
-  exec python "tests.py"
+  exec python "test.py"
 else
   echo "Server Production"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/aplikasi.py --callable app --stats 0.0.0.0:9191
